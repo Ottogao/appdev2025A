35096f1ae06a287942a8296ecd3b5b373d9f0bfe
Chao Gao <gc@vamk.fi>
2025-10-28 08:45:35 +0200

    Add project files.

---
+++ pascal/pascal.h
@@ -0,0 +1,9 @@
+#pragma once
+// there are usually 3 parts in a header file: 
+//	1) constant declaration
+//	2) struct/class definition
+//	3) function declaration
+
+// function declarations
+int factorial(int n);
+int n_choose_k(int n, int k);
